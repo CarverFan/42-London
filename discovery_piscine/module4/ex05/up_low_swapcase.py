b828483ab592cb90bca7628c7ee71a4640963a40
CarverFan <54744412+CarverFan@users.noreply.github.com>
2026-02-19 16:21:35 +0000

    Use swapcase()

---
+++ discovery_piscine/module4/ex05/up_low_swapcase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Discovery Piscine
+# Module4 - Python
+# The program should:
+# 	Prompt the user to enter a sttring
+#	Display the string with uppercase letters changed to lower case
+#	and vice versa
+
+text = input()
+
+print(text.swapcase())
+
