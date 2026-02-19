e5030388f3292d97270baf363e8dd7927fc6c5d1
CarverFan <54744412+CarverFan@users.noreply.github.com>
2026-02-19 15:54:18 +0000

    Simplified

---
+++ discovery_piscine/module6/ex03/aff_rev_params_simplified.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# Discovery Piscine
+# Module6 - Python
+# The program should:
+#	When executed, the program should display all the strings passed as parameters,
+#	followed by a newline, in reverse order.
+#	If there are fewer than two parameters, it shoud display "none" followed by a
+#	newline.
+
+import sys
+
+if len(sys.argv) > 2:
+	for i in reversed(sys.argv[1:]):
+		print(i)
+else:
+	print("none")
+
